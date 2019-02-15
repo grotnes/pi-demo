22cf00f1c327d2005b35e8772867bebc27341dfd
Erik Grotnes <erik@grotnes.net>
2019-02-15 22:08:09 +0000

    Added smilefjes

---
+++ smilefjes.py
@@ -0,0 +1,34 @@
+from sense_hat import SenseHat
+import time
+#import socket
+
+sense = SenseHat()
+
+import netifaces as ni
+ni.ifaddresses('eth0')
+ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+print ip  # should print "192.168.100.37"
+#print dir(ni)
+print ni.interfaces()
+
+for interface in ni.interfaces():
+	if interface == "lo":
+		continue
+
+	print "Found interface " + interface
+	ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+	print "IP = " + ip
+	sense.show_message(interface + ":" + ip)
+
+time.sleep(2)
+
+sense.set_pixel(2, 2, (0, 0, 255))
+sense.set_pixel(4, 2, (0, 0, 255))
+sense.set_pixel(3, 4, (100, 0, 0))
+sense.set_pixel(1, 5, (255, 0, 0))
+sense.set_pixel(2, 6, (255, 0, 0))
+sense.set_pixel(3, 6, (255, 0, 0))
+sense.set_pixel(4, 6, (255, 0, 0))
+sense.set_pixel(5, 5, (255, 0, 0))
+
+
