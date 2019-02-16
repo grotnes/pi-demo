8f318bf0756a90a6ce581647e218b811897b2316
Erik Grotnes <erik@grotnes.net>
2019-02-16 16:07:07 +0000

    Joystick loop

--- smilefjes.py
+++ smilefjes.py
@@ -5,13 +5,19 @@ from time import sleep
 sense = SenseHat()
 sense.low_light = True
 
-def write_ip_addr(event):
-	if event.action != 'pressed':
-		return
+# Define global variables
+active = "dot" # Active subprogram
+busy   = False  # Status
+
+def write_ip_addr():
+	print("Starts Write IP addr")
+	global busy, active
+	busy = True
+	active = ""
 	import netifaces as ni
-	ni.ifaddresses('eth0')
-	ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
-	print ip  # should print "192.168.100.37"
+	#ni.ifaddresses('eth0')
+	#ip = ni.ifaddresses('eth0')[ni.AF_INET][0]['addr']
+	#print ip  # should print "192.168.100.37"
 	#print dir(ni)
 	print ni.interfaces()
 
@@ -20,13 +26,20 @@ def write_ip_addr(event):
 			continue
 	
 		print "Found interface " + interface
-		ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
-		print "IP = " + ip
-		sense.show_message(interface + ":" + ip)
+		print(ni.ifaddresses(interface))
+		try:
+			ip = ni.ifaddresses(interface)[ni.AF_INET][0]['addr']
+			print "IP = " + ip
+			sense.show_message(interface + ":" + ip)
+		except:
+			pass
+	busy = False
 
-def smilefjes(event):
-	if event.action != 'pressed':
-		return
+def smilefjes():
+	global busy, active
+	print("Starts Smilefjes.")
+	busy = True
+	active = ""
 	sense.set_pixel(2, 2, (0, 0, 255))
 	sense.set_pixel(4, 2, (0, 0, 255))
 	sense.set_pixel(3, 4, (100, 0, 0))
@@ -35,6 +48,7 @@ def smilefjes(event):
 	sense.set_pixel(3, 6, (255, 0, 0))
 	sense.set_pixel(4, 6, (255, 0, 0))
 	sense.set_pixel(5, 5, (255, 0, 0))
+	busy = False
 
 def init_dot():
 	# Draws the screen background for the "dot" program
@@ -60,6 +74,10 @@ def init_dot():
 	
 def dot():
 	# Make a dot that moves when you flip
+	global busy, active
+	print("Starts Dot")
+	busy = True
+	active = "Dot"
 	init_dot()
 	x_calc = 3 # Float number. Knowing cursors exact position
 	y_calc = 3 # Float number. Knowing cursors exact position
@@ -68,8 +86,9 @@ def dot():
 	x_previous = 3 # Integer, knowing previous cursor position
 	y_previous = 3 # Integer, knowing previous cursor position
 	previous_color = sense.get_pixel(x_previous, y_previous)
+	busy = False
 
-	while True:
+	while active == "Dot":
 		acceleration = sense.get_accelerometer_raw()
 		x = acceleration['x']
 		y = acceleration['y']
@@ -103,7 +122,7 @@ def dot():
 			change = 0
 
 		#print("x_pos={0}, y_pos={1}, change={2}, x={3}, y={4}, z={5}".format(x_pos, y_pos, change, x, y, z))
-		print("x_pos={0}, y_pos={1}, change={2}, x_prev={3}, y_prev={4}".format(x_pos, y_pos, change, x_calc, y_calc))
+		#print("x_pos={0}, y_pos={1}, change={2}, x_prev={3}, y_prev={4}".format(x_pos, y_pos, change, x_calc, y_calc))
 
 		if (change == 1):
 			sense.set_pixel(x_previous, y_previous, previous_color)
@@ -113,12 +132,47 @@ def dot():
 			y_previous = y_pos
 
 
-# Make "write_ip_addr" run when joystick pushed up
-sense.stick.direction_up = write_ip_addr
+def handle_joystick(event):
+	global active, busy
+	if event.action == 'pressed':# and busy == False:
+		if event.direction == 'up':
+			active = "IP"
+		if event.direction == 'down':
+			active = "Smilefjes"
+		if event.direction == 'left':
+			active = "Dot"
+		if event.direction == 'right':
+			active = "Blue"
+		if event.direction == 'middle':
+			active = "Quit"
 
-# Make "smilefjes" run when joystick pushed down
-sense.stick.direction_down = smilefjes
+def blue():
+	print("Starts Blue")
+	sense.clear((0,0,255))
 
-#write_ip_addr()
-#smilefjes()
-dot()
+# Make handles for joystick events
+sense.stick.direction_up     = handle_joystick
+sense.stick.direction_down   = handle_joystick
+sense.stick.direction_left   = handle_joystick
+sense.stick.direction_right  = handle_joystick
+sense.stick.direction_middle = handle_joystick
+
+busy = False
+active = "Dot"
+while True:
+	if active == "IP":
+		sense.clear((0,0,255))
+		write_ip_addr()
+	if active == "Smilefjes":
+		sense.clear((0,0,255))
+		smilefjes()
+	if active == "Dot":
+		sense.clear((0,0,255))
+		dot()
+	if active == "Blue":
+		blue()
+	if active == "Quit":
+		sense.clear((0,0,255))
+		quit()
+	print("Venter paa input")
+	sleep(0.5)
